75358e731c8b923e5cc85b37e436a5153aa9e958
pyw424 <parkyw0424@naver.com>
2026-02-04 17:47:36 +0900

    #1 ui: 헤더 푸터 컴포넌트 생성

---
+++ src/shared/components/Footer.tsx
@@ -0,0 +1,16 @@
+function Footer() {
+  return (
+    <footer className="bg-black2 text-gray2 mt-auto w-full px-10 py-12 text-[14px]">
+      <div className="mx-auto max-w-[1440px] space-y-4">
+        <p>© 2026 LIKELION KNU. All rights reserved.</p>
+        <p className="text-gray7 leading-5 whitespace-pre-line">
+          {`실습실 : 경기도 용인시 기흥구 강남로 40 강남대학교 후생관 104호
+            동아리실 : 경기도 용인시 기흥구 강남로 40 강남대학교 후생관 멋쟁이사자처럼
+            프로젝트 개발 : 프로젝트 코어`}
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
