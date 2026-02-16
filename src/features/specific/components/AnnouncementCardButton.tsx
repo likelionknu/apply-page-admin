7ea13f940d30b9d29539accb9cc4c4cbc11a8de5
chanju <chanju030303@naver.com>
2026-02-16 19:54:45 +0900

    #15 feat: 상세 공고 페이지 성레이아웃 완

--- src/features/specific/components/AnnouncementCardButton.tsx
+++ src/features/specific/components/AnnouncementCardButton.tsx
@@ -1,12 +1,53 @@
+import Modal from "@shared/components/Modal";
+import Button from "@shared/components/Button";
+import { useState } from "react";
+
 interface AnnouncementButtonProps {
   text: string;
+  modalTitle: string;
+  modalDescription: string;
 }
 
-const AnnouncementButton = ({ text }: AnnouncementButtonProps) => {
+const AnnouncementButton = ({
+  text,
+  modalTitle,
+  modalDescription,
+}: AnnouncementButtonProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="bg-admin-box flex h-9 w-28 cursor-pointer items-center justify-center rounded-[10px] text-sm font-medium text-white hover:opacity-70">
-      {text}
-    </div>
+    <>
+      <div
+        className="bg-admin-box flex h-9 w-28 cursor-pointer items-center justify-center rounded-[10px] text-sm font-medium text-white hover:opacity-70"
+        onClick={() => setIsOpen(true)}
+      >
+        {text}
+      </div>
+
+      {isOpen && (
+        <Modal>
+          <Modal.TextLayout>
+            <Modal.Title>{modalTitle}</Modal.Title>
+            <Modal.Description>{modalDescription}</Modal.Description>
+          </Modal.TextLayout>
+
+          <Modal.ButtonLayout>
+            <button onClick={() => setIsOpen(false)} className="flex w-full">
+              <Button>취소</Button>
+            </button>
+
+            <div
+              onClick={() => {
+                setIsOpen(false);
+              }}
+              className="flex w-full"
+            >
+              <Button>확인</Button>
+            </div>
+          </Modal.ButtonLayout>
+        </Modal>
+      )}
+    </>
   );
 };
 
--- src/features/specific/components/AnnouncementDelete.tsx
+++ src/features/specific/components/AnnouncementDelete.tsx
@@ -1,36 +1,46 @@
 import Modal from "@shared/components/Modal";
 import Button from "@shared/components/Button";
 
-const a = () => {
-  return;
-};
+import { useState } from "react";
 
 export const AnnouncementDelete = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <>
-      <div className="bg-admin-box flex h-9 w-24 cursor-pointer items-center justify-center rounded-[10px] text-center text-sm font-medium text-white hover:opacity-70">
+      <div
+        onClick={() => setIsOpen(true)}
+        className="bg-admin-box flex h-9 w-24 cursor-pointer items-center justify-center rounded-[10px] text-center text-sm font-medium text-white hover:opacity-70"
+      >
         공고 삭제
       </div>
 
-      <Modal>
-        <Modal.TextLayout>
-          <Modal.Title>사용자 권한 변경</Modal.Title>
-          <Modal.Description>
-            사용자의 권한을 변경할까요? {"1n"}이 작업은 즉시 적용되며, 되돌릴 수
-            없어요.
-          </Modal.Description>
-        </Modal.TextLayout>
+      {isOpen && (
+        <Modal>
+          <Modal.TextLayout>
+            <Modal.Title>사용자 권한 변경</Modal.Title>
+            <Modal.Description>
+              이 공고에 지원한 사용자(임시저장 상태 포함)가 존재한다면 이 작업은
+              거부될 수 있어요
+            </Modal.Description>
+          </Modal.TextLayout>
 
-        <Modal.ButtonLayout>
-          <button onClick={() => a()} className="flex w-full">
-            <Button>취소</Button>
-          </button>
-          / flex.w-full
-          <div onClick={() => a()} className="flex w-full">
-            <Button>확인</Button>
-          </div>
-        </Modal.ButtonLayout>
-      </Modal>
+          <Modal.ButtonLayout>
+            <button onClick={() => setIsOpen(false)} className="flex w-full">
+              <Button>취소</Button>
+            </button>
+
+            <div
+              onClick={() => {
+                setIsOpen(false);
+              }}
+              className="flex w-full"
+            >
+              <Button>확인</Button>
+            </div>
+          </Modal.ButtonLayout>
+        </Modal>
+      )}
     </>
   );
 };
