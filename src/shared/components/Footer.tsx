fd636249b96d6c7c22d91d047604bc01d1f33c9b
pyw424 <parkyw0424@naver.com>
2026-02-16 19:38:13 +0900

    #1 ui: footer 수정

--- src/shared/components/Footer.tsx
+++ src/shared/components/Footer.tsx
@@ -1,9 +1,11 @@
-function Footer() {
+export default function Footer() {
   return (
-    <footer className="bg-black2 text-gray2 mt-auto w-full px-10 py-12 text-[14px]">
-      <div className="mx-auto max-w-[1440px] space-y-4">
-        <p>© 2026 LIKELION KNU. All rights reserved.</p>
-        <p className="text-gray7 leading-5 whitespace-pre-line">
+    <footer className="bg-admin-box text-gray2 mt-75 w-full">
+      <div className="mx-auto max-w-360 space-y-2 px-10 py-12 text-[14px]">
+        <p className="text-admin-white">
+          © 2026 LIKELION KNU. All rights reserved.
+        </p>
+        <p className="text-admin-sub leading-7 whitespace-pre-line">
           {`실습실 : 경기도 용인시 기흥구 강남로 40 강남대학교 후생관 104호
             동아리실 : 경기도 용인시 기흥구 강남로 40 강남대학교 후생관 멋쟁이사자처럼
             프로젝트 개발 : 프로젝트 코어`}
@@ -12,5 +14,3 @@ function Footer() {
     </footer>
   );
 }
-
-export default Footer;
