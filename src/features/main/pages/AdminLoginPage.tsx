851322ababd14a77ca19621a3604bb8c4597523f
pyw424 <parkyw0424@naver.com>
2026-02-16 19:26:47 +0900

    #1 ui: 메인 로그인 페이지 수정

--- src/features/main/pages/AdminLoginPage.tsx
+++ src/features/main/pages/AdminLoginPage.tsx
@@ -1,29 +1,29 @@
+import Footer from "@shared/components/Footer";
 import { Link } from "react-router-dom";
-import logoImg from "../../../shared/assets/loginlogo.png";
-import googleImg from "../../../shared/assets/google.png";
-import linkImg from "../../../shared/assets/link.png";
 
-import Footer from "../../../shared/components/Footer";
+import googleImg from "@shared/assets/google.png";
+import linkImg from "@shared/assets/link.png";
+import mainLogoImg from "@shared/assets/loginlogo.png";
 
 function AdminLoginPage() {
   return (
-    <div className="bg-black1 text-white1 flex w-full flex-col">
+    <div className="bg-admin-background flex w-full flex-col text-white">
       <div className="mx-auto mt-30 flex min-h-screen w-full max-w-360 flex-col items-center gap-6">
         <div className="flex flex-col">
           <div className="flex items-center gap-3">
-            <img src={logoImg} alt="logo" className="h-[40px] w-[28px]" />
+            <img src={mainLogoImg} alt="logo" className="h-[40px] w-[28px]" />
             <p className="text-[40px] leading-none font-semibold">
               LIKELION KNU
             </p>
           </div>
-          <p className="pr-[2px] text-right text-[16px] text-gray-400">
+          <p className="pr-[2px] text-right text-[16px] text-white">
             운영진 전용 페이지
           </p>
         </div>
 
-        <div className="bg-black3 flex h-[179px] w-full max-w-[521px] flex-col items-center justify-center rounded-[10px] border border-none">
+        <div className="bg-admin-box flex h-[179px] w-full max-w-[521px] flex-col items-center justify-center rounded-[10px] border border-none">
           <Link
-            to="/admin/user-dashboard"
+            to="/admin/user-list"
             className="border-gray2 hover:bg-black2 m-5 flex cursor-pointer items-center rounded-lg border-[0.4px] px-5 py-2.5 text-white transition"
           >
             <img src={googleImg} alt="google" className="w-6" />
@@ -33,7 +33,7 @@ function AdminLoginPage() {
           </Link>
           <Link
             to={"https://apply-page-client.vercel.app/"}
-            className="text-purple flex items-center justify-center hover:cursor-pointer"
+            className="text-admin-blue flex items-center justify-center hover:cursor-pointer"
           >
             <p className="mr-4">아기사자 지원페이지를 찾고 있나요?</p>
             <img
