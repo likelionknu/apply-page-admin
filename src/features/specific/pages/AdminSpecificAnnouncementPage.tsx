c005a08a811d8f0f19ff8a98b83694f4fe201c7c
chanju <chanju030303@naver.com>
2026-02-16 21:27:49 +0900

    .

--- src/features/specific/pages/AdminSpecificAnnouncementPage.tsx
+++ src/features/specific/pages/AdminSpecificAnnouncementPage.tsx
@@ -8,21 +8,7 @@ import AdminStateButton from "@specific/components/AdminStatusButton";
 import AnnouncementButton from "@specific/components/AnnouncementCardButton";
 import AnnouncementDateText from "@specific/components/AnnouncementDateText";
 import { AnnouncementDelete } from "@specific/components/AnnouncementDelete";
-
-const AnnouncementQuestion = () => {
-  return (
-    <div className="flex w-full items-center gap-3">
-      <div className="bg-admin-box flex h-9 w-28 items-center justify-between rounded-[10px] px-3.5 py-2.5">
-        <div className="justify-start text-sm font-medium text-[#5A5A5A]">
-          순서
-        </div>
-      </div>
-      <div className="bg-admin-box flex h-9 w-full items-center rounded-[10px] px-7.5 py-2.5 text-base font-medium text-white">
-        멋쟁이사자처럼 강남대학교에 어떤 계기로 지원하게 되었나요?
-      </div>
-    </div>
-  );
-};
+import { AnnouncementQuestion } from "@specific/components/AnnouncementQuestion";
 
 interface AnnouncementTitleTextProps {
   text: string;
